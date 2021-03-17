957d5a6aa3dba0cb010c8ef8ea4b0ed124bcd81c
AgustinLarranagaMoby <alarranaga@mobydigital.com>
2021-03-17 12:47:47 -0300

    primer commit

---
+++ src/main/java/com/equiposmoby/equiposmoby/EquiposmobyApplication.java
@@ -0,0 +1,13 @@
+package com.equiposmoby.equiposmoby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposmobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquiposmobyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/equiposmoby/equiposmoby/EquiposmobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equiposmoby.equiposmoby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiposmobyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
