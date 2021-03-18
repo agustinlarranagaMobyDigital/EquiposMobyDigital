e1720b38d6d74719a2e5432948559bf676d5bebc
AgustinLarranagaMoby <alarranaga@mobydigital.com>
2021-03-18 14:25:15 -0300

    creo clase programador

---
+++ src/main/java/com/equiposmoby/equiposmoby/Models/Entity/Programador.java
@@ -0,0 +1,10 @@
+package com.equiposmoby.equiposmoby.Models.Entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Programador extends Integrante{
+
+    public Programador() {
+    }
+}
