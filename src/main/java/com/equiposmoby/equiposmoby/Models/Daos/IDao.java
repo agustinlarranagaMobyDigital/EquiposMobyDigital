2c0debd6df28d6a1acfdeca59db77f6e5c823108
Solera <martin@mobydigital.com>
2021-03-18 15:54:20 -0300

    Correciones de la interfaz generica

--- src/main/java/com/equiposmoby/equiposmoby/Models/Daos/IDao.java
+++ src/main/java/com/equiposmoby/equiposmoby/Models/Daos/IDao.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IDao<T,t>{
+    /**Metodos de Reunion */
+
     List<T> traerTodas();
     void agregar(T t);
     void delete(T t);
+    void guardar();
 
-
+    /**TipoReunion*/
 
 
 }
