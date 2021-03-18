f03414d1ba27357d821ca7020a6f2d02c3483a2d
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-03-18 15:11:46 -0300

    actualizacion de UserController

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/UserController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
-import com.equiposmoby.equiposmoby.Models.Daos.IUserDao;
+import com.equiposmoby.equiposmoby.Models.Entity.User;
+import com.equiposmoby.equiposmoby.Services.IUsuarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @Autowired
-    private IUserDao userDao;
+    private IUsuarioServices userService;
 
     @RequestMapping("/listado")
     public String listar(Model model){
         model.addAttribute("titulo" , "Listado de Usuarios");
-        model.addAttribute("usuarios" , userDao.obtenerLista());
+        model.addAttribute("usuarios" , userService.traerTodas());
         return "listar-usuarios";
     }
+
+    @RequestMapping("/buscar")
+    public String buscar(Model model){
+        model.addAttribute("titulo" , "Buscar Isaias");
+        model.addAttribute("usuario" , userService.buscar("isaias", "isaias"));
+        return "buscar-uno";
+    }
+
+    @RequestMapping("/agregar")
+    public String agregar(Model model){
+        User isaias = new User();
+        isaias.setEmail("isaias@isaias.com");
+        isaias.setPassword("isaias22");
+        userService.agregar(isaias);
+        model.addAttribute("titulo" , "Agregar");
+        model.addAttribute("usuarios" , userService.traerTodas());
+        return "listar-usuarios";
+    }
+
+
 }
