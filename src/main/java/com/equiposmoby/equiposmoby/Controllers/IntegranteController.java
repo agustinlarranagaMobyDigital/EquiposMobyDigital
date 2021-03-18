975dac6fd87c2b2469d64d18b3978f99811a0064
AgustinLarranagaMoby <alarranaga@mobydigital.com>
2021-03-18 15:34:23 -0300

    integrante controller

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/IntegranteController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/IntegranteController.java
@@ -1,7 +1,8 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
 import com.equiposmoby.equiposmoby.Models.Entity.Integrante;
-import com.equiposmoby.equiposmoby.Models.Entity.Programador;
+import com.equiposmoby.equiposmoby.Models.Entity.Lenguaje;
+import com.equiposmoby.equiposmoby.Models.Entity.Puesto;
 import com.equiposmoby.equiposmoby.Services.IntegranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class IntegranteController {
 
     private String titulo = "Equipos Moby Digital!";
+
     @Autowired
     private IntegranteService integranteService;
 
@@ -22,9 +26,14 @@ public class IntegranteController {
 
         Integrante integrante = new Integrante();
 
+        List<Lenguaje> lenguajes = integranteService.getLenguajes();
+        List<Puesto> puestos = integranteService.getPuestos();
+
         model.addAttribute("titulo",titulo);
         model.addAttribute("h1","Formulario del nuevo empleado de Moby Digital!");
         model.addAttribute("integrante",integrante);
+        model.addAttribute("lenguajes",lenguajes);
+        model.addAttribute("puestos",puestos);
 
         return "formIntegrante";
     }
@@ -33,10 +42,9 @@ public class IntegranteController {
     @PostMapping(value = "/addIntegrante")
     public String addIntegrante(Model model,  Integrante integrante ){
 
-        Programador programador = integrante;
-        integranteService
+        integranteService.add(integrante);
 
-        return "redirect:listarClientes";
+        return "redirect:formIntegrante";
     }
 
 }
