79cab25d0954e6ea9371d7d05c8a67f66988963e
AgustinLarranagaMoby <alarranaga@mobydigital.com>
2021-03-18 14:09:29 -0300

    haceindo import de algunos datos para integrante

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into puestos (nombre) values ( 'frontend' );
+insert into puestos (nombre) values ( 'backend' );
+insert into puestos (nombre) values ( 'lider' );
+
+insert into lenguajes (nombre) values ( 'java' );
+insert into lenguajes (nombre) values ( 'javaScript' );
+insert into lenguajes (nombre) values ( 'html' );
+insert into lenguajes (nombre) values ( 'php' );
+insert into lenguajes (nombre) values ( 'css' );
