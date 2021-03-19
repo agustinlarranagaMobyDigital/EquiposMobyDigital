9d9945eff0cdbad12fe647580672708809064b4f
Solera <martin@mobydigital.com>
2021-03-19 15:34:57 -0300

    Corrigiendo errores de ReunionController

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/ReunionController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/ReunionController.java
@@ -1,19 +1,21 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
-import com.equiposmoby.equiposmoby.Models.Daos.IDao;
+import com.equiposmoby.equiposmoby.Models.Daos.ReunionDaoImple;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class ReunionController {
 
     @Autowired
-    private IDao reunionDao;
+    private ReunionDaoImple reunionDao;
 
-    @RequestMapping(value = "/reuniones") // Si esta vacio, implicitamente es GET
+    @RequestMapping( "/reuniones") // Si esta vacio, implicitamente es GET
     public String listar(Model model){
         model.addAttribute("titulo","Listado de las reuniones");
-        model.addAttribute("reunion", reunionDao.traerTodas());
-        return "reuniones";
+        model.addAttribute("reuniones", reunionDao.traerTodas());
+        return "listar-reuniones";
     }
 }
