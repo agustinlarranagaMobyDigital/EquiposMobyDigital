f2930c8b79aedd28fec5c23b98d2e08cef4c425a
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-03-19 17:08:39 -0300

    Creacion de editors

---
+++ src/main/java/com/equiposmoby/equiposmoby/Models/Editors/PuestoPropertieEditor.java
@@ -0,0 +1,30 @@
+package com.equiposmoby.equiposmoby.Models.Editors;
+
+import com.equiposmoby.equiposmoby.Services.IntegranteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.beans.PropertyEditorSupport;
+
+@Component
+public class PuestoPropertieEditor extends PropertyEditorSupport {
+
+    @Autowired
+    private IntegranteService integranteService;
+
+    @Override
+    public void setAsText(String idString) throws IllegalArgumentException {
+
+        if (idString != null && idString.length() > 0){
+            try {
+                Integer id = Integer.parseInt(idString);
+                this.setValue(integranteService.getPuestoByID(id));
+            }catch (NumberFormatException e){
+                setValue(null);
+            }
+        }else{
+            setValue(null);
+        }
+
+    }
+}
