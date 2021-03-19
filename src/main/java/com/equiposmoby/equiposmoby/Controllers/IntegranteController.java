56317735ce22fbe1576509dcf5da1519b130cfb8
Isaias <isaiasemanuelcalfin@hotmail.com>
2021-03-19 17:05:28 -0300

    Actualizacion integranteController

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/IntegranteController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/IntegranteController.java
@@ -1,18 +1,26 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
+import com.equiposmoby.equiposmoby.Models.Editors.LenguajePropertieEditor;
+import com.equiposmoby.equiposmoby.Models.Editors.PuestoPropertieEditor;
 import com.equiposmoby.equiposmoby.Models.Entity.Integrante;
 import com.equiposmoby.equiposmoby.Models.Entity.Lenguaje;
 import com.equiposmoby.equiposmoby.Models.Entity.Puesto;
+import com.equiposmoby.equiposmoby.Models.Entity.User;
 import com.equiposmoby.equiposmoby.Services.IntegranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IntegranteController {
@@ -22,18 +30,32 @@ public class IntegranteController {
     @Autowired
     private IntegranteService integranteService;
 
+    @Autowired
+    private PuestoPropertieEditor puestoPropertieEditor;
+    @Autowired
+    private LenguajePropertieEditor lenguajePropertieEditor;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        binder.registerCustomEditor(Puesto.class,"puesto",puestoPropertieEditor);
+        binder.registerCustomEditor(Lenguaje.class,"lenguajes",lenguajePropertieEditor);
+    }
+
 
     @RequestMapping(value = "/formIntegrante")
     public String formIntegrante (Model model){
 
         Integrante integrante = new Integrante();
 
-        List<Puesto> listaPuestos = integranteService.getPuestos();
+        List<Puesto> listaPuestos= integranteService.getPuestos();
+
         List<Lenguaje> listaLenguajes = integranteService.getLenguajes();
 
         model.addAttribute("titulo",titulo);
         model.addAttribute("h1","Formulario del nuevo empleado de Moby Digital!");
         model.addAttribute("integrante",integrante);
+        model.addAttribute("listaPuestos",listaPuestos);
+        model.addAttribute("listaLenguajes",listaLenguajes);
 
 
         return "formIntegrante";
@@ -41,27 +63,40 @@ public class IntegranteController {
 
 
     @PostMapping(value = "/addIntegrante")
-    public String addIntegrante(Model model, Integrante integrante){
+    public String addIntegrante(@Valid Integrante integrante,BindingResult result, Model model, 
+                                @RequestParam String email,@RequestParam String password){
 
 
-        List<Lenguaje> listaLenguajes = integranteService.getLenguajes();
-        integrante.setLenguajes(listaLenguajes);
+        
+        // INCORPORAR A LA CLASE INTEGRANTE
+        // 1.usuario 2.equipo 3.agenda 4.lenguajes 5.puesto
+        Map<String,String> errores = integranteService.crearUsuario(result,email,password);
+        System.out.println("errores = " + errores);
+        if(errores.isEmpty()){
 
-        List<Integrante> listaparaLenguajes = new ArrayList<>();
-        listaparaLenguajes.add(integrante);
-        for (int i = 0; i < listaLenguajes.size(); i++) {
-            listaLenguajes.get(i).setIntegrantes(listaparaLenguajes);
+            //1. usuario -->  bien
+            User user = integranteService.getUltimoUserByEmail(email);
+            integrante.setUsuario(user);
+            // 5. puesto --> bien
+            if(integrante.getPuesto().getNombre().equals("lider")){
+                integrante.setJefe(true);
+            }
+
+            //PRUEBAS !!!!
+            System.out.println("integrante" + integrante.toString());
+
+            /*List<Lenguaje> listaLenguajes = integranteService.getLenguajes();
+            integrante.setLenguajes(listaLenguajes);
+
+            integranteService.add(integrante);
+*/
+            return  "redirect:/formIntegrante";
+        }else{
+            model.addAttribute("errores", errores);
+            return "formIntegrante";
         }
 
-        System.out.println("listaLenguajes = " +  integrante.getLenguajes().toString());
 
-        List<Puesto> listaPuestos = integranteService.getPuestos();
-        integrante.setPuesto(listaPuestos.get(1));
-
-
-      //  integranteService.add(integrante);
-
-        return "redirect:formIntegrante";
     }
 
 }
