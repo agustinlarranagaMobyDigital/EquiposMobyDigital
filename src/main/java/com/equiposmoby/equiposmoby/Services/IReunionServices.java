ad81267c153bfa0067ca18b851b0ed9181f61fa3
Solera <martin@mobydigital.com>
2021-03-19 16:46:49 -0300

    Creando Interfaz de reunion services

--- src/main/java/com/equiposmoby/equiposmoby/Services/IReunionServices.java
+++ src/main/java/com/equiposmoby/equiposmoby/Services/IReunionServices.java
@@ -1,2 +1,14 @@
-package com.equiposmoby.equiposmoby.Services;public interface IReunionServices {
+package com.equiposmoby.equiposmoby.Services;
+
+import com.equiposmoby.equiposmoby.Models.Entity.Reunion;
+
+import java.util.List;
+
+public interface IReunionServices {
+
+    public List<Reunion> traerTodas();
+    public void agregar(Reunion reunion);
+    public void eliminar(Reunion reunion);
+    public Reunion buscar(String txt);
+
 }
