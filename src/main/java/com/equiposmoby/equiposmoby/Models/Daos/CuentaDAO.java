b80e5af67257a2efcf5b5a87b4703d9cb35fb461
Solera <martin@mobydigital.com>
2021-03-22 15:32:40 -0300

    Cuenta DAO

--- src/main/java/com/equiposmoby/equiposmoby/Models/Daos/CuentaDAO.java
+++ src/main/java/com/equiposmoby/equiposmoby/Models/Daos/CuentaDAO.java
@@ -1,2 +1,42 @@
-package com.equiposmoby.equiposmoby.Models.Daos;public class CuentaDAO {
+package com.equiposmoby.equiposmoby.Models.Daos;
+
+import com.equiposmoby.equiposmoby.Models.Entity.Cuenta;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository(value = "cuentaDaoJPA")
+public class CuentaDAO implements IDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public List<Cuenta> traerTodas() {
+        return em.createQuery("from Cuenta").getResultList();
+    }
+
+    @Override
+    public void agregar(Object o) {
+
+    }
+
+    @Override
+    public void eliminar(Object o) {
+
+    }
+
+    @Override
+    public Cuenta buscar(String txt) {
+        return em.find(Cuenta.class,txt);
+    }
+
+
+
+    @Override
+    public Cuenta getById(Integer id) {
+        return null;
+    }
 }
