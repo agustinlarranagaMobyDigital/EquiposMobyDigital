2b594cee33b7248e7b7441965cb0dd43b70acb68
Gianni <gricciardi@mobydigital.com>
2021-03-22 14:57:22 -0300

    Actualización ticket-020
    desarrollo equipoService

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/EquipoController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/EquipoController.java
@@ -1,12 +1,27 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
+import com.equiposmoby.equiposmoby.Models.Entity.Equipo;
 import com.equiposmoby.equiposmoby.Services.EquipoServiceIMP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class EquipoController {
 
     @Autowired
     private EquipoServiceIMP equipoServiceIMP;
+
+    @RequestMapping(value = "/formAgregarEquipo")
+    public String formAgregarEquipo (Model model){
+
+        Equipo equipo = new Equipo();
+
+        List<Equipo>
+
+        return "formAgregarIntegrante";
+    }
 }
