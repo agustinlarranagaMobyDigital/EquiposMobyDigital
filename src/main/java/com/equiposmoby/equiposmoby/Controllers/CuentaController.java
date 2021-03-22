c4905153a240f2084d5c081cc00e14c7fb206350
Solera <martin@mobydigital.com>
2021-03-22 15:33:10 -0300

    Cuenta Controller

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/CuentaController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/CuentaController.java
@@ -1,2 +1,28 @@
-package com.equiposmoby.equiposmoby.Controllers;public class CuentaController {
+package com.equiposmoby.equiposmoby.Controllers;
+
+import com.equiposmoby.equiposmoby.Services.CuentaService;
+import com.equiposmoby.equiposmoby.Services.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class CuentaController {
+
+    @Autowired
+    private CuentaService cuentaService;
+
+    @Autowired
+    private SessionService sessionService;
+
+    @RequestMapping("/cuenta")
+    public String listar(Model model, HttpSession session){
+        model.addAttribute("titulo","listado de cuentas");
+        model.addAttribute("cuentas",cuentaService.traerTodas());
+        return sessionService.sesionIniciada(session,"listar-cuentas");
+    }
+
 }
