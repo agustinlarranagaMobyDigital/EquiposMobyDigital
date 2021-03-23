b6e62eae70bb46d82c7950a416e237c654995689
MartinGil <mgil@mobydigital.com>
2021-03-23 13:08:29 -0300

    Se creo el metodo de agregarReunion con su respectivo endpoint

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/ReunionController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/ReunionController.java
@@ -1,13 +1,16 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
-import com.equiposmoby.equiposmoby.Models.Daos.ReunionDaoImple;
+import com.equiposmoby.equiposmoby.Models.Entity.Reunion;
 import com.equiposmoby.equiposmoby.Services.ReunionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller()
 public class ReunionController {
 
     @Autowired
@@ -18,6 +21,12 @@ public class ReunionController {
         model.addAttribute("titulo","Listado de las reuniones");
         model.addAttribute("reuniones", reunionService.traerTodas());
         return "listar-reuniones";
+    }
+
+    @RequestMapping(value = "/reunion/" ,method = RequestMethod.POST)
+    public ResponseEntity agregarReunion (@RequestBody Reunion reunion){
+
+        return ResponseEntity.ok(reunionService.agregarReunion(reunion));
     }
 }
 
