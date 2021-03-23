6724ee948cf016a2249c550bea403d1eb9ba8639
MartinGil <mgil@mobydigital.com>
2021-03-23 13:04:32 -0300

    Se realizo el metodo agregarReunion y se modifico nombre de la variable reunionDAO

--- src/main/java/com/equiposmoby/equiposmoby/Services/ReunionService.java
+++ src/main/java/com/equiposmoby/equiposmoby/Services/ReunionService.java
@@ -3,7 +3,6 @@ package com.equiposmoby.equiposmoby.Services;
 import com.equiposmoby.equiposmoby.Models.Daos.ReunionDaoImple;
 import com.equiposmoby.equiposmoby.Models.Entity.Reunion;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,25 +11,40 @@ import java.util.List;
 public class ReunionService implements IReunionServices {
 
     @Autowired
-    private ReunionDaoImple ReunionDAO;
+    private ReunionDaoImple reunionDAO;
 
     @Override
     public List<Reunion> traerTodas() {
-        return ReunionDAO.traerTodas();
+        return reunionDAO.traerTodas();
     }
 
     @Override
     public void agregar(Reunion reunion) {
-        ReunionDAO.agregar(reunion);
+
+        reunionDAO.agregar(reunion);
     }
 
     @Override
     public void eliminar(Reunion reunion) {
-        ReunionDAO.eliminar(reunion);
+        reunionDAO.eliminar(reunion);
     }
 
     @Override
     public Reunion buscar(String txt) {
-        return ReunionDAO.buscar(txt);
+        return reunionDAO.buscar(txt);
+    }
+
+    public Reunion agregarReunion(Reunion reunion){
+
+
+        Reunion reunionNew = Reunion.builder()
+                .idReunion(reunion.getIdReunion())
+                .fecha(reunion.getFecha())
+                .horaInicial(reunion.getHoraInicial())
+                .horaFinal(reunion.getHoraFinal())
+                .tipoReunion(reunion.getTipoReunion())
+                .build();
+        agregar(reunionNew);
+        return reunionNew;
     }
 }
