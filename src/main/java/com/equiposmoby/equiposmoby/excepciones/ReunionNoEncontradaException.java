290e09f99ee40450b6a78dd89047df1a8f7b78c8
MartinGil <mgil@mobydigital.com>
2021-03-26 11:00:48 -0300

    Se agrego la excepcion ReunionNoEncontrada

---
+++ src/main/java/com/equiposmoby/equiposmoby/excepciones/ReunionNoEncontradaException.java
@@ -0,0 +1,6 @@
+package com.equiposmoby.equiposmoby.excepciones;
+
+public class ReunionNoEncontradaException extends RuntimeException {
+
+    public ReunionNoEncontradaException(){super("No se encontro la reunion");}
+}
