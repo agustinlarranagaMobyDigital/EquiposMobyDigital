0f7b1659e38066f5aa62d64a45b59e01a66081be
MartinGil <mgil@mobydigital.com>
2021-03-26 10:47:46 -0300

    Se agrego el metodo para la respuesta de la exception de ReunionNoEncontrada

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/AdviceController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/AdviceController.java
@@ -3,6 +3,7 @@ package com.equiposmoby.equiposmoby.Controllers;
 import com.equiposmoby.equiposmoby.Models.respuestas.ErrorResponse;
 import com.equiposmoby.equiposmoby.excepciones.AgendaNoCreadaException;
 import com.equiposmoby.equiposmoby.excepciones.AgendaNoEncontradaException;
+import com.equiposmoby.equiposmoby.excepciones.ReunionNoEncontradaException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +22,13 @@ public class AdviceController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(AgendaNoCreadaException.class)
     public ErrorResponse handleAgendaNoCreadaException(AgendaNoCreadaException ex) {
+
+        return ErrorResponse.fromRunTimeException(ex);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ReunionNoEncontradaException.class)
+    public ErrorResponse handleReunionNoEncontradaException(ReunionNoEncontradaException ex) {
 
         return ErrorResponse.fromRunTimeException(ex);
     }
