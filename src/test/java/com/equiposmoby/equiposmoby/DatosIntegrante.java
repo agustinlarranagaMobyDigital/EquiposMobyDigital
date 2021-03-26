a8168b99324a41c83eced5afaddcb7a3960131f6
AgustinLarranagaMoby <alarranaga@mobydigital.com>
2021-03-26 10:32:31 -0300

    agregando test al integrante service

--- src/test/java/com/equiposmoby/equiposmoby/DatosIntegrante.java
+++ src/test/java/com/equiposmoby/equiposmoby/DatosIntegrante.java
@@ -1,2 +1,36 @@
-package com.equiposmoby.equiposmoby;public class DatosIntegrante {
+package com.equiposmoby.equiposmoby;
+
+import com.equiposmoby.equiposmoby.Models.Entity.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class DatosIntegrante {
+
+    public final static List<Lenguaje> LENGUAJES = Arrays.asList(
+                                                                new Lenguaje(1,"java"),
+                                                                new Lenguaje(2,"php"),
+                                                                new Lenguaje(3,"css"),
+                                                                new Lenguaje(4,"phyton"));
+
+
+    public final static Integrante INTEGRANTE = new Integrante(1,"agustin","larra", new Date(),1,false,
+                                                                    new Equipo(),new Puesto(),
+                                                                    Arrays.asList( new Lenguaje(1,"java"), new Lenguaje(2,"php")),
+                                                                    new Agenda(),new User());
+
+    public final static List<Puesto> PUESTOS = Arrays.asList(
+            new Puesto(1,"tester"),
+            new Puesto(2,"front"),
+            new Puesto(3,"back"),
+            new Puesto(4,"lider"));
+
+
+    public final static List<Equipo> EQUIPOS = Arrays.asList(
+            new Equipo(1,"trainies"),
+            new Equipo(2,"rocket"),
+            new Equipo(3,"proyecto interno"),
+            new Equipo(4,"icbc"));
 }
--- src/test/java/com/equiposmoby/equiposmoby/IntegranteTest.java
+++ src/test/java/com/equiposmoby/equiposmoby/IntegranteTest.java
@@ -1,2 +1,90 @@
-package com.equiposmoby.equiposmoby;public class IntegranteTest {
+package com.equiposmoby.equiposmoby;
+
+import com.equiposmoby.equiposmoby.Controllers.IntegranteController;
+import com.equiposmoby.equiposmoby.Models.Daos.EquipoDAO;
+import com.equiposmoby.equiposmoby.Models.Daos.IntegranteDAO;
+import com.equiposmoby.equiposmoby.Models.Daos.LenguajeDAO;
+import com.equiposmoby.equiposmoby.Models.Daos.PuestoDAO;
+import com.equiposmoby.equiposmoby.Models.Entity.Equipo;
+import com.equiposmoby.equiposmoby.Models.Entity.Integrante;
+import com.equiposmoby.equiposmoby.Models.Entity.Lenguaje;
+import com.equiposmoby.equiposmoby.Services.EquipoServiceIMP;
+import com.equiposmoby.equiposmoby.Services.IntegranteService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class IntegranteTest {
+
+    @Mock
+    PuestoDAO puestoDAO;
+
+    @Mock
+    LenguajeDAO lenguajeDAO;
+
+    @Mock
+    Lenguaje lenguaje;
+
+    @Mock
+    EquipoDAO equipoDAO;
+
+    @Mock
+    IntegranteDAO integranteDAO;
+
+    @InjectMocks
+    IntegranteController controller;
+
+    @InjectMocks
+    IntegranteService service;
+
+
+
+
+
+    @Test
+    void getEquipoByID_TestEXITOSO(){
+        //given
+        Equipo equipo = new Equipo(1,"trainies");
+        when(equipoDAO.traerTodas()).thenReturn(DatosIntegrante.EQUIPOS);
+        //when
+        Equipo buscado =service.getEquipoByID(1);
+        //then
+        assertNotNull(buscado);
+        assertEquals(equipo,buscado);
+    }
+
+    @Test
+    void getEquipoByID_Test_EQUIPONULL(){
+        //given
+        Equipo equipo = mock(Equipo.class);
+        when(equipoDAO.traerTodas()).thenReturn(DatosIntegrante.EQUIPOS);
+        //when
+        Equipo buscado =service.getEquipoByID(100);
+        //then
+        assertNull(buscado);
+        assertNotEquals(equipo,buscado);
+    }
+
+    @Test
+    void getEquipoByID_Test_IDMAL(){
+        //given
+        Equipo equipo = mock(Equipo.class);
+        //when
+        Equipo buscado =service.getEquipoByID(-1);
+        //then
+        assertNull(buscado);
+        assertNotEquals(equipo,buscado);
+    }
 }
