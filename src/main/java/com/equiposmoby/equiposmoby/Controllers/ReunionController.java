3d579a55f72606a7fd0ae9ec1782a3e77ead9fb7
Solera <martin@mobydigital.com>
2021-03-29 09:53:53 -0300

    Actualizando reunionController

--- src/main/java/com/equiposmoby/equiposmoby/Controllers/ReunionController.java
+++ src/main/java/com/equiposmoby/equiposmoby/Controllers/ReunionController.java
@@ -1,27 +1,48 @@
 package com.equiposmoby.equiposmoby.Controllers;
 
 import com.equiposmoby.equiposmoby.Models.Daos.ReunionDaoImple;
+import com.equiposmoby.equiposmoby.Models.Entity.Cuenta;
+import com.equiposmoby.equiposmoby.Models.Entity.Reunion;
 import com.equiposmoby.equiposmoby.Services.ReunionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ReunionController {
 
+    private String titulo = "Reuniones MobyDigital";
+
     @Autowired
     private ReunionService reunionService;
 
-    @RequestMapping( "/reuniones") // Si esta vacio, implicitamente es GET
-    public String listar(Model model){
-        model.addAttribute("titulo","Listado de las reuniones");
-        model.addAttribute("reuniones", reunionService.traerTodas());
-        return "listar-reuniones";
+
+    @RequestMapping(value = "/agregarReunion")
+    public String addReunion(Model model){
+        model.addAttribute("titulo","Agregar Reunion");
+        model.addAttribute("agregar",reunionService.agregar(new Reunion()));
+        return "agregar-reunion";
     }
+
+    @RequestMapping( "/listarReuniones") // Si esta vacio, implicitamente es GET
+    public String listarReuniones(Model model, Integer id){
+        model.addAttribute("listar", reunionService.traerTodas());
+        return "listar-reunion";
+    }
+
+
+
+
+
+
 }
-
-
 
 
 
